14db9334a189e2b8994603ebc90a49c5c8997985
SnubMansters <you.know.whos.cool@gmail.com>
2026-04-04 01:05:35 -0400

    Times change
    added a time of day system.
    fixed a few bugs
    money earned was op
    a few things under the hood but we will get through this

--- sw.js
+++ sw.js
@@ -1,33 +1,7 @@
-const CACHE = 'undeadinc-v1';
-const ASSETS = [
-  './game_mobile.html',
+const CACHE='undeadinc-v2';
+const ASSETS=['./game_mobile.html',
   'https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Special+Elite&family=Oswald:wght@300;400&display=swap',
-  'https://cdnjs.cloudflare.com/ajax/libs/d3/7.9.0/d3.min.js'
-];
-
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE).then(cache => cache.addAll(ASSETS).catch(() => {}))
-  );
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    )
-  );
-  self.clients.claim();
-});
-
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(cached => cached || fetch(e.request).then(res => {
-      if (!res || res.status !== 200) return res;
-      const clone = res.clone();
-      caches.open(CACHE).then(cache => cache.put(e.request, clone));
-      return res;
-    }).catch(() => cached))
-  );
-});
+  'https://cdnjs.cloudflare.com/ajax/libs/d3/7.9.0/d3.min.js'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS).catch(()=>{})));self.skipWaiting();});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
+self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(cached=>cached||fetch(e.request).then(res=>{if(!res||res.status!==200)return res;const clone=res.clone();caches.open(CACHE).then(c=>c.put(e.request,clone));return res;}).catch(()=>cached)));});
