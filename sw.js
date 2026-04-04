02c042fe4070134050a762154d98ee6ea8d3fb44
SnubMansters <you.know.whos.cool@gmail.com>
2026-04-04 01:28:56 -0400

    I shouldn't be awake...
    fixed prices in the shop.
    discounts never go below 10%
    sniper rifles now can pierce through up to 2 zombies

--- sw.js
+++ sw.js
@@ -1,7 +1,5 @@
-const CACHE='undeadinc-v2';
-const ASSETS=['./game_mobile.html',
-  'https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Special+Elite&family=Oswald:wght@300;400&display=swap',
-  'https://cdnjs.cloudflare.com/ajax/libs/d3/7.9.0/d3.min.js'];
+const CACHE='undeadinc-v3';
+const ASSETS=['./game_mobile.html','https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Special+Elite&family=Oswald:wght@300;400&display=swap','https://cdnjs.cloudflare.com/ajax/libs/d3/7.9.0/d3.min.js'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS).catch(()=>{})));self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
-self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(cached=>cached||fetch(e.request).then(res=>{if(!res||res.status!==200)return res;const clone=res.clone();caches.open(CACHE).then(c=>c.put(e.request,clone));return res;}).catch(()=>cached)));});
+self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{if(!res||res.status!==200)return res;const c=res.clone();caches.open(CACHE).then(ca=>ca.put(e.request,c));return res;}).catch(()=>r)));});
