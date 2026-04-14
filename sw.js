94a548bce0e0317f88ea55dbc6aea639a7c806fe
SnubMansters <you.know.whos.cool@gmail.com>
2026-04-14 12:24:10 -0400

    omw to work
    fixed a bug in the main menu preventing the game from starting
    added a funny little thing
    
    thats all?

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='undeadinc-v7';
+const CACHE='undeadinc-v8';
 const ASSETS=['./game_mobile.html','https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Special+Elite&family=Oswald:wght@300;400&display=swap','https://cdnjs.cloudflare.com/ajax/libs/d3/7.9.0/d3.min.js'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS).catch(()=>{})));self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
